ec5d4eff6e6e0a8e78238cd787fdde5ffc782ff1
bashlikovvv <volodia413314@gmail.com>
2023-09-28 19:06:35 +0300

    SQLDelight support added.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     kotlin("multiplatform")
+    kotlin("plugin.serialization")
     id("com.android.library")
     id("org.jetbrains.compose")
     id("kotlin-parcelize")
+    id("app.cash.sqldelight")
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
@@ -30,6 +32,7 @@ kotlin {
                     implementation(components.resources)
                     implementation(compose.uiTooling)
                     implementation(compose.preview)
+                    implementation(Dependencies.AndroidX.ConstraintLayout.constraintLayoutCompose)
                 }
 
                 with(Dependencies.Io.InsertKoin) {
@@ -47,18 +50,35 @@ kotlin {
                     implementation(decompose)
                     implementation(extensionsCompose)
                 }
+
+                with(Dependencies.App.Cash.SQLDelight) {
+                    implementation(runtime)
+                    implementation(coroutinesExtensions)
+                }
+
+                implementation(Dependencies.Com.Arkivanov.Essenty.lifecycle)
+                implementation(Dependencies.Org.JetBrains.KotlinX.serializationJson)
             }
         }
         val androidMain by getting {
             dependencies {
                 implementation(Dependencies.AndroidX.Core.core)
                 implementation(Dependencies.Io.InsertKoin.koinAndroid)
+                implementation(Dependencies.App.Cash.SQLDelight.androidDriver)
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
             }
+        }
+    }
+}
+
+sqldelight {
+    databases {
+        create("TracksDatabase") {
+            packageName.set("by.bashlikovvv.music_player.tracksDatabase")
         }
     }
 }
