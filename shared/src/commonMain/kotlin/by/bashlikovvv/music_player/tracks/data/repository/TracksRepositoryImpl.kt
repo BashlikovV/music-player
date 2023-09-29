4e92223d4e3a02ebb798c11b3e45ad20e6522716
bashlikovvv <volodia413314@gmail.com>
2023-09-29 21:18:36 +0300

    loadTracks method modified.

--- shared/src/commonMain/kotlin/by/bashlikovvv/music_player/tracks/data/repository/TracksRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/by/bashlikovvv/music_player/tracks/data/repository/TracksRepositoryImpl.kt
@@ -1,15 +1,19 @@
 package by.bashlikovvv.music_player.tracks.data.repository
 
+import by.bashlikovvv.music_player.core.data.StorageScanner
 import by.bashlikovvv.music_player.tracks.data.exception.CanNotDeleteFileException
 import by.bashlikovvv.music_player.tracks.data.mapper.CommonDirectoryEntityMapper
 import by.bashlikovvv.music_player.tracks.data.mapper.DirectoryMapper
+import by.bashlikovvv.music_player.tracks.data.mapper.FileToDirectoryMapper
 import by.bashlikovvv.music_player.tracks.data.model.DirectoryEntity
 import by.bashlikovvv.music_player.tracks.domain.model.Track
 import by.bashlikovvv.music_player.tracks.domain.repository.IDirectoriesRepository
 import by.bashlikovvv.music_player.tracks.domain.repository.ITracksRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.supervisorScope
 import java.io.File
 
@@ -17,25 +21,42 @@ class TracksRepositoryImpl(
     private val directoriesRepository: IDirectoriesRepository
 ) : ITracksRepository {
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun loadTracks(limit: Int, offset: Int): Flow<List<Track>> {
-        return directoriesRepository.getDirectories().map { directoryEntities ->
+        val storageScanner = StorageScanner()
+
+        val tmpRes = directoriesRepository.getDirectories().mapLatest { directoryEntities ->
+            var id = 0
+
             supervisorScope {
+                val tracks = mutableListOf<Track>()
+
                 directoryEntities.map {
                     async {
-                        DirectoryMapper().toDomain(
-                            CommonDirectoryEntityMapper().toDomain(it as DirectoryEntity.CommonDirectoryEntity)
-                        )
+                        storageScanner.scanDirectory(
+                            directory = CommonDirectoryEntityMapper().toDomain(it as DirectoryEntity.CommonDirectoryEntity),
+                            limit = limit,
+                            offset = offset
+                        ).map {
+                            DirectoryMapper().toDomain(
+                                FileToDirectoryMapper(id).toDomain(it).also { id++ }
+                            )
+                        }
                     }
-                }.map { it.await() }
+                }.map { tracks.addAll(it.await()) }
+
+                tracks
             }
         }
+
+        return tmpRes
     }
 
     /**
      * @throws CanNotDeleteFileException if track file not found
      * */
     override suspend fun deleteTrack(track: Track) {
-        val tracks = directoriesRepository.getDirectories().map { directoryEntities ->
+        val tracksFlow = directoriesRepository.getDirectories().map { directoryEntities ->
             directoryEntities.map {
                 DirectoryMapper().toDomain(
                     CommonDirectoryEntityMapper().toDomain(it as DirectoryEntity.CommonDirectoryEntity)
@@ -43,7 +64,7 @@ class TracksRepositoryImpl(
             }.map { it }
         }
 
-        tracks.map { tracks ->
+        tracksFlow.map { tracks ->
             tracks.forEach {
                 if (it == track) {
                     try {
