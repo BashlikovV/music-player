f1b15d277267438cd6a413c748a7a827ea8d038c
bashlikovvv <volodia413314@gmail.com>
2023-09-30 16:27:06 +0300

    SettingsScreen added.

---
+++ shared/src/commonMain/kotlin/by/bashlikovvv/music_player/tracks/ui/settings/ui/SettingsScreen.kt
@@ -0,0 +1,10 @@
+package by.bashlikovvv.music_player.tracks.ui.settings.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import by.bashlikovvv.music_player.tracks.ui.settings.SettingsComponent
+
+@Composable
+fun SettingsScreen(component: SettingsComponent) {
+    Text("Settings")
+}
