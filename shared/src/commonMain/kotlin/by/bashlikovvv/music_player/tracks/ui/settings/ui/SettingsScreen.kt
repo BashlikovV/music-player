ae17a0b73f38964082b0f5cc54bafefac74d3164
bashlikovvv <volodia413314@gmail.com>
2023-09-30 21:40:07 +0300

    SettingsScreen updated.

--- shared/src/commonMain/kotlin/by/bashlikovvv/music_player/tracks/ui/settings/ui/SettingsScreen.kt
+++ shared/src/commonMain/kotlin/by/bashlikovvv/music_player/tracks/ui/settings/ui/SettingsScreen.kt
@@ -1,10 +1,18 @@
 package by.bashlikovvv.music_player.tracks.ui.settings.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import by.bashlikovvv.music_player.tracks.ui.settings.SettingsComponent
 
 @Composable
 fun SettingsScreen(component: SettingsComponent) {
-    Text("Settings")
+    Scaffold {
+        Column(modifier = Modifier.padding(it)) {
+            Text("Settings")
+        }
+    }
 }
