0a327f184c7bf12d83da1233a308db2ae7a532c5
bashlikovvv <volodia413314@gmail.com>
2023-09-30 21:40:52 +0300

    ModalBottomSheetTopBar added.

---
+++ shared/src/commonMain/kotlin/by/bashlikovvv/music_player/tracks/ui/browser/ui/modal_bottom_sheet/ModalBottomSheetTopBar.kt
@@ -0,0 +1,22 @@
+package by.bashlikovvv.music_player.tracks.ui.browser.ui.modal_bottom_sheet
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.layoutId
+
+@Composable
+fun ModalBottomSheetTopBar() {
+    Row(
+        modifier = Modifier.fillMaxWidth().height(50.dp).layoutId("sheetTopBar"),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+    }
+}
