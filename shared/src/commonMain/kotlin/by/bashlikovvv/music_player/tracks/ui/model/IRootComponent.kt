6561aa189cd59f96405aae338597ec4a3a119ed9
bashlikovvv <volodia413314@gmail.com>
2023-09-30 16:25:24 +0300

    Settings screen added.

--- shared/src/commonMain/kotlin/by/bashlikovvv/music_player/tracks/ui/model/IRootComponent.kt
+++ shared/src/commonMain/kotlin/by/bashlikovvv/music_player/tracks/ui/model/IRootComponent.kt
@@ -2,6 +2,7 @@ package by.bashlikovvv.music_player.tracks.ui.model
 
 import by.bashlikovvv.music_player.tracks.ui.chooser.DirectoriesChooserComponent
 import by.bashlikovvv.music_player.tracks.ui.explorer.ExplorerComponent
+import by.bashlikovvv.music_player.tracks.ui.settings.SettingsComponent
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.value.Value
 
@@ -12,6 +13,7 @@ interface IRootComponent {
     sealed class Child {
         class ExplorerChild(val component: ExplorerComponent) : Child()
         class ChooserChild(val component: DirectoriesChooserComponent) : Child()
+        class SettingsChild(val component: SettingsComponent) : Child()
     }
 
 }
